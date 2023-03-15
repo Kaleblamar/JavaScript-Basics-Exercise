6e4dd476338452d4c72f882eeed8e7b76faf127d
Kaleb Lamar <lamarkaleb@gmail.com>
2023-03-15 09:29:26 -0500

    JS basics Exercise

---
+++ script.js
@@ -0,0 +1,50 @@
+//1
+console.log(22);
+
+//2
+// numbers, strings, null,undefined, boolean
+
+//3
+// +, -, /, *, %, **
+
+//4
+console.log(5 % 4);
+console.log(5 % 2);
+
+//5
+console.log("I'M MARRY POPPINS Y'ALL");
+
+//6
+console.log("hawk" + "eye");
+
+//7
+console.log("Kaleb".length + "Lamar".length);
+
+//8
+"Avengers: Endgame"[16];
+
+//BONUS
+//1
+console.log("\\");
+
+//2
+console.log(5 ** 4 - 9);
+
+//3
+console.log("I".length - "Am".length + ("Iron".length % "Man".length));
+
+//4
+console.log("CAP"[1]);
+console.log("Vision"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR_LORD"[0]);
+
+//5
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u);
